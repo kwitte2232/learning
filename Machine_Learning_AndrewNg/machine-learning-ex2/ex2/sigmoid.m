712610a8f2fb9bcc803b4c1a2fd26e165ef66ea1
kwitte2232 <kwitte@uchicago.edu>
2017-09-29 14:25:18 -0500

    Exercise 2: Logistic Regression

---
+++ Machine_Learning_AndrewNg/machine-learning-ex2/ex2/sigmoid.m
@@ -0,0 +1,22 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+negative_z = -z;
+g = (1 + exp(negative_z)).^-1;
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+
+
+
+
+% =============================================================
+
+end
